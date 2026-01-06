837e6bceceb01b8b0746896c531a8b14668ec2e2
Jason Davis <jason@notthatjason.com>
2026-01-06 11:33:29 -0500

    feat: added doctrine and db connection info
    tweaking bootstrap / autoload still necessary

--- public/index.php
+++ public/index.php
@@ -3,7 +3,28 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+
+$config = ORMSetup::createAttributeMetadataConfig(
+  paths: [__DIR__ . '/src'],
+  isDevMode: true,
+);
+
+$connection_info = [
+  'driver' => 'pdo_pgsql',
+  'host' => 'localhost',
+  'dbname' => 'coverdb',
+  'user' => '',
+  'password' => '',
+];
+
+$connection = DriverManager::getConnection($connection_info);
+
 
 $app = AppFactory::create();
 
---
+++ src/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+// bootstrap
+
+# use Psr\Http\Message\ResponseInterface as Response;
+# use Psr\Http\Message\ServerRequestInterface as Request;
+# use Slim\Factory\AppFactory;
+# 
+# use Doctrine\DBAL\DriverManager;
+# use Doctrine\ORM\EntityManager;
+# use Doctrine\ORM\ORMSetup;
+
+# require __DIR__ . '/../vendor/autoload.php';
