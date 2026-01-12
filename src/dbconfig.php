f66670add4f379ed008bf1ec81809e910d99eadf
Jason Davis <jason@notthatjason.com>
2026-01-11 19:46:48 -0500

    fix: resolve proxy path exception

--- src/dbconfig.php
+++ src/dbconfig.php
@@ -1,16 +1,36 @@
 <?php
 
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\Mapping\Driver\AttributeDriver;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+
+// TODO: alter these later for prod deploy
+$isDev = true;
+$driverImpl = new AttributeDriver([__DIR__ . '/../src/Entities']);
+$queryCache = new ArrayAdapter();
+$metadataCache = new ArrayAdapter();
 
 
-$config = ORMSetup::createAttributeMetadataConfig(
-  paths: [__DIR__ . '/../src'],
-  isDevMode: $isDev,
-);
+$config = new Configuration;
+$config->setMetadataCache($metadataCache);
+$config->setMetadataDriverImpl($driverImpl);
+$config->setQueryCache($queryCache);
 
-$connection_info = [
+$proxyDir = 'data/DoctrineORMModule/Proxy';
+
+$config->setProxyDir(__DIR__ . '/../'  . $proxyDir);
+$config->setProxyNamespace('Proxies');
+
+// $config = ORMSetup::createAttributeMetadataConfig(
+//   paths: [__DIR__ . '/../src'],
+//   isDevMode: $isDev,
+//   
+// );
+
+$postgres_info = [
   'driver' => 'pdo_pgsql',
   // 'host' => $_ENV['DB_HOST'],
   'host' => 'posty',
@@ -20,7 +40,7 @@ $connection_info = [
 //   'sslmode' => $_ENV['SSL_MODE'],
 ];
 
-$conn = DriverManager::getConnection($connection_info);
+$conn = DriverManager::getConnection($postgres_info);
 
 $entityMgr = new EntityManager($conn, $config);
 
