610c7a9409158c88f1eec2c1e251fec2faea2616
Jason Davis <jason@notthatjason.com>
2026-01-30 23:57:07 -0500

    wip: writing users to the page

--- public/js/index.js
+++ public/js/index.js
@@ -11,26 +11,46 @@ const makeApiCall = async (endpoint) => {
     }
 
     const json = await response.json();
-    return json;
+    return Success(json);
   } catch (error) {
     return Failure(error.message);
   }
 };
 
+// effectPipe wrapping ceremony to handle makeApiCall via Command
 const getData = (input) => {
   const cmdApiCall = () => makeApiCall(input.endpoint);
   const next = (data) => Success(data);
   return Command(cmdApiCall, next);
+};
+
+const writePageData = (dataWrapper) => {
+
+  const ul = document.querySelector('#workers ul');
+
+
+  return Success(dataWrapper.value);
 }
 
+const writeData = (input) => {
+  if (input.type !== 'Success') {
+    return input;
+  }
+
+  const writeDataCall = () => writePageData(input);
+  const next = (data) => Success(data);
+
+  return Command(writeDataCall, next);
+};
+
 const getUserDataFlow = (input) => effectPipe(
-    () =>getData(input)
+    getData,
+    writeData
+  // add function for writing data to page
   )(input);
     
-async function loadPageData() {
-
+async function loadWorkerData() {
   const logic = getUserDataFlow({endpoint: 'account' });
-
   const result = await runEffect(logic);
 
   if (result.type === 'Success') {
@@ -40,6 +60,8 @@ async function loadPageData() {
   if (result.type !== 'Success') {
     console.error('Error:', result.error);
   }
-}
+};
 
-loadPageData();
+loadWorkerData();
+
+
